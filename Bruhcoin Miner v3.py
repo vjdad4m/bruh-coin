1c233596554ff0025beae93f90111234cc6243bc
Adam Vajda <75809430+vjdad4m@users.noreply.github.com>
2021-03-05 00:00:40 +0100

    Add files via upload

--- Bruhcoin Miner v3.py
+++ Bruhcoin Miner v3.py
@@ -6,7 +6,7 @@ import pyfiglet
 v = '3.0'
 
 print()
-result = pyfiglet.figlet_format("BruhCoin", font = "larry3d") 
+result = pyfiglet.figlet_format("BruhCoin", font = "larry3d")
 print(result,v)
 
 print('-----------------------------------------------------------------')
@@ -36,11 +36,11 @@ def mine(s,diff=1):
         end_t = datetime.now()
         end = end_t.strftime("%H:%M:%S")
         elapsed = (end_t - start_t)
-        s = f"{nonce} | Diff: {diff} | Hash: {hash(s+str(nonce))} | Start: {start} | End: {end} | Elapsed: {elapsed}\n"
+        y = f"{nonce} | Diff: {diff} | Hash: {hash(s+str(nonce))} | Start: {start} | End: {end} | Elapsed: {elapsed}\n"
         if should_save:
             with open("results.txt", "a") as myfile:
-                myfile.write(s)
-        print(s)
+                myfile.write(y)
+        print(y)
         nonce+=1
         
 mine('bruhcoin',diff)
